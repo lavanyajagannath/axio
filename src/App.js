6512771e4c2d7386319e27430ee4bb7b69f7eaf7
lavanyajagannath <jlavanya064@gmail.com>
2022-07-21 21:47:21 +0530

    fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css"
 
-function App() {
+const App = () => {
+  const [data, setData] = useState({});
+
+  let fetchData = async () => {
+    let fetchedItem = await axios.get(
+      "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
+    );
+    setData(fetchedItem.data["Time Series (5min)"]);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>fetched Data</h1>
+      <br />
+      <table>
+        <thead>
+          <tr className="tr" style={{borderBottom: '1px solid black'}}> 
+            <th>DateTime</th>
+            <th>Open</th>
+            <th>high</th>
+            <th>low</th>
+            <th>close</th>
+            <th>volume</th>
+          </tr>
+        </thead>
+        <tbody>
+      {/* iterating object using object.keys() method which gives an array*/}
+          {Object.keys(data).map((key,index) => {
+            let item = data[key];
+
+            return (
+              <tr key={index}>
+                <td>{key}</td>
+                <td>{item["1. open"]}</td>
+                <td>{item["2. high"]}</td>
+                <td>{item["3. low"]}</td>
+                <td>{item["4. close"]}</td>
+                <td>{item["5. volume"]}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
